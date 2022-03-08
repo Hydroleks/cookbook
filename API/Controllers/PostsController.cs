3e3236e131add919f4764d33648a87ef7fd0f8ec
Aleksandar Stojanovic <leksandar.stojanovic@gmail.com>
2022-03-08 20:28:26 +1100

    Very small refactor. Moved Mediator to base controller.

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,21 +1,14 @@
 using API.Controllers;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using MediatR;
 using Application.Posts;
 
 public class PostsController : BaseApiController
 {
-    private readonly IMediator _mediator;
-    public PostsController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpGet]
     public async Task<ActionResult<List<Post>>> GetPostsAsync()
     {
-        return await _mediator.Send(new List.Query());
+        return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
