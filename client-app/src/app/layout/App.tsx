fa26a783859237ba6f6d58171a1dc3c70d8bcea6
Aleksandar Stojanovic <leksandar.stojanovic@gmail.com>
2022-03-10 20:28:32 +1100

    Added interface for Post.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Post } from '../models/post';
 
 function App() {
 
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    axios.get("https://localhost:5001/api/Posts").then(response => {
+    axios.get<Post[]>("https://localhost:5001/api/Posts").then(response => {
       console.log(response);
       setPosts(response.data);
     });
@@ -17,7 +18,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Posts'/>
       <List>
-        {posts.map((post: any) => (
+        {posts.map(post => (
             <List.Item key={post.id}>
               {post.title}
             </List.Item>
