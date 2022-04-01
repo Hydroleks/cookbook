1be45b4f602ed040745422a1d25d4e9aa505528e
Aleksandar Stojanovic <leksandar.stojanovic@gmail.com>
2022-04-01 20:08:40 +1100

    Moved posts list and initial loading to post store.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Post } from '../models/post';
 import NavBar from './NavBar';
 import PostDashboar from '../../features/posts/dashboard/PostDashboard';
@@ -15,15 +15,11 @@ function App() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [selectedPost, setSelectedPost] = useState<Post|undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Posts.list().then(response => {
-      setPosts(response);
-      setLoading(false);
-    });
-  }, []);
+    postStore.loadPosts();
+  }, [postStore]);
 
   function handleSelectPost(id: string) {
     setSelectedPost(posts.find(post => post.id === id));
@@ -75,7 +71,7 @@ function App() {
     })
   }
 
-  if(loading) return <LoadingComponent content='Loading App' />
+  if(postStore.initialLoading) return <LoadingComponent content='Loading App' />
 
   return (
     // Fragment groups things together, so we can return 2 things without the use of an empty div.
@@ -83,12 +79,8 @@ function App() {
     <> 
       <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: '7em'}}>
-
-        <h2>{postStore.title}</h2>
-        <Button content="Modify Title" positive onClick={postStore.setTitle} />
-
         <PostDashboar 
-          posts={posts}
+          posts={postStore.posts}
           selectedPost={selectedPost}
           selectPost={handleSelectPost}
           cancelSelectPost={handleCanelSelectPost}
--- client-app/src/app/stores/postStore.ts
+++ client-app/src/app/stores/postStore.ts
@@ -1,13 +1,32 @@
 import { makeAutoObservable } from "mobx";
+import agent from "../api/agent";
+import { Post } from "../models/post";
 
 export default class PostStore {
-    title = 'MobX set up and ready to go.';
+    posts: Post[] = [];
+    selectedPost: Post | null = null;
+    editMode = false;
+    loading = false;
+    initialLoading = false;
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    setTitle = () => {
-        this.title += '!';
+    loadPosts = async () => {
+        this.initialLoading = true;
+        try {
+            const posts = await agent.Posts.list();
+            posts.forEach(post => {
+                // We might want to keep the full time stamp as well as the date.
+                post.created = post.created.split('T')[0];
+                post.modified = post.modified.split('T')[0];
+                this.posts.push(post);
+            })
+            this.initialLoading = false;
+        } catch(error) {
+            console.log(error);
+            this.initialLoading = false;
+        }
     }
 }
