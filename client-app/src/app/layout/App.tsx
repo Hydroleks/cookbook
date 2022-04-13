a22f95ac97b709d25a0253d53d907f124bc606a1
Aleksandar Stojanovic <leksandar.stojanovic@gmail.com>
2022-04-13 16:06:05 +1000

    Added home page. Split posts into posts and create routes.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { Post } from '../models/post';
 import NavBar from './NavBar';
 import PostDashboar from '../../features/posts/dashboard/PostDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import PostForm from '../../features/posts/form/PostForm';
 
 function App() {
-  const {postStore} = useStore();
-
-  useEffect(() => {
-    postStore.loadPosts();
-  }, [postStore]);
-
-  if(postStore.initialLoading) return <LoadingComponent content='Loading App' />
-
   return (
     // Fragment groups things together, so we can return 2 things without the use of an empty div.
     // Shortcut version using Fragment is just empty <> as below.
     <> 
       <NavBar/>
       <Container style={{marginTop: '7em'}}>
-        <PostDashboar />
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/posts' component={PostDashboar}/>
+        <Route exact path='/createPost' component={PostForm}/>
       </Container>
     </>
   );
--- client-app/src/features/posts/dashboard/PostDashboard.tsx
+++ client-app/src/features/posts/dashboard/PostDashboard.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
-import react from 'react';
+import react, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import PostDetails from '../details/PostDetails';
 import PostForm from '../form/PostForm';
@@ -9,6 +10,12 @@ import PostList from './PostList';
 export default observer(function PostDashboar() {
     const {postStore} = useStore();
     const {selectedPost, editMode} = postStore;
+
+    useEffect(() => {
+      postStore.loadPosts();
+    }, [postStore]);
+  
+    if(postStore.initialLoading) return <LoadingComponent content='Loading App' />
 
     return (
         <Grid>
